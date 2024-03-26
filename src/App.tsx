af9e8bbb2ab0c67c90cf6dc429b93967dc919391
harshana-lk <harshuonlive@gmail.com>
2024-03-26 13:54:23 +0530

    🔖Passing functions via Props

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,18 @@ const App = () => {
     "Paris",
     "Berlin",
   ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 };
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-const ListGroup = ({ items, heading }: Props) => {
+const ListGroup = ({ items, heading, onSelectItem }: Props) => {
   //   Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -24,6 +25,7 @@ const ListGroup = ({ items, heading }: Props) => {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}

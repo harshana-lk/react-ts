8940f72d88302a440e341090d7c47768a7286afe
harshana-lk <harshuonlive@gmail.com>
2024-03-26 14:05:41 +0530

    🔖Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 const App = () => {
-  let items = [
-    "New York",
-    "San Francisco",
-    "Tokyo",
-    "London",
-    "Paris",
-    "Berlin",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello World <span>Hello Hello</span>
+      </Alert>
     </div>
   );
 };

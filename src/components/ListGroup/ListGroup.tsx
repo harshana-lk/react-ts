b3a846a31fa59546f215bd7947d14a3bc8916ad1
harshana-lk <harshuonlive@gmail.com>
2024-03-27 13:07:20 +0530

    ♻️CSS in TS

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,6 +1,22 @@
 import { useState } from "react";
-import styles from "./ListGroup.module.css";
-import "../../App.css";
+import "./ListGroup.css";
+import styled from "styled-components";
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  cursor: pointer;
+  background: ${(props) => (props.active ? "blue" : "none")};
+  color: ${(props) => (props.active ? "white" : "black")};
+`;
 
 interface Props {
   items: string[];
@@ -10,20 +26,16 @@ interface Props {
 
 const ListGroup = ({ items, heading, onSelectItem }: Props) => {
   //   Hook
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No items</p>}
-      <ul className={[styles.listGroup, styles.container].join(" ")}>
+      <List>
         {items.map((item, index) => (
-          <li
-            className={
-              selectedIndex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
+          <ListItem
+            active={index === selectedIndex}
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -31,9 +43,9 @@ const ListGroup = ({ items, heading, onSelectItem }: Props) => {
             }}
           >
             {item}
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
